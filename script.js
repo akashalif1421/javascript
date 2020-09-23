f475d4e93eddf06e1b737908b63844bae73b452c
akashalif1421 <71547840+akashalif1421@users.noreply.github.com>
2020-09-23 18:50:46 +0600

    helllo

---
+++ script.js
@@ -0,0 +1,117 @@
+alert("Akash alif :Welcome To My Site!")
+
+
+
+
+
+//console
+var X = "Welcome to my site";
+var Y = 2020;
+var Z = true;
+var d = ["html", "css", "javacsript"];
+var e = {
+    firstname: "Sadia",
+    lastname: "None"
+};
+document.write(X);
+document.write("<br>");
+document.write(typeof X);
+document.write("<br>");
+document.write(typeof Y);
+document.write("<br>");
+document.write(typeof Z);
+document.write("<br>");
+document.write(typeof d);
+document.write("<br>");
+document.write(typeof e);
+document.write("<br>");
+
+
+
+
+
+//alif
+var day = 1;
+switch (day) {
+    case 1:
+        document.write("today is Monday")
+        document.write("<br>")
+
+        break;
+    case 2:
+        document.write("today is Sunday")
+        document.write("<br>")
+        break;
+    case 3:
+        document.write("today is Suterday")
+        document.write("<br>")
+        break;
+    case 4:
+        document.write("today is Tuesday")
+        document.write("<br>")
+        break;
+    case 5:
+        document.write("today is Wednesday")
+        document.write("<br>")
+        break;
+    case 6:
+        document.write("today is Thursday")
+        document.write("<br>")
+        break;
+    case 7:
+        document.write("today is Fryday")
+        document.write("<br>")
+        break;
+    default:
+        document.write("Wrong Valid")
+        document.write("<br>")
+        break;
+}
+
+
+var h=10;
+var i=20;
+if (h<i) {
+    alert("he is graeter")
+    document.write("<br>")
+}else{
+    alert("she is graeter")
+    document.write("<br>")
+}
+
+
+
+//open
+ function hello(){
+    document.write("Hello world! ");
+    document.write("<br>");
+}
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+
+var a= 1;
+while (a<=10) {
+    document.write("Welcome");
+    a=a+1;
+}
+var a= 1;
+while (a<=10) {
+    document.write( a + "Welcome <br>");
+    a=a+1;
+}
+var a= 1;
+while (a<=10) {
+    document.write( a + "Hey <br>");
+    a=a+2;
+}
